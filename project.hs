d90b4ac221f7ec2c82dbad1aee9432da030c7094
Piotr <piterek007@o2.pl>
2018-01-21 14:52:11 +0100

    szyfruje liczby
    wersja pierwsza szyfruje inty, w sposób bardzo prosty, ale działający. Zaraz biorę się za słowa :)

--- project.hs
+++ project.hs
@@ -5,11 +5,24 @@ encoding = do
     txt <- getLine
     putStr "Wczytano: "
     putStrLn (txt)
-    let num = read txt :: Int
+    putStrLn "Podaj liczbe kodujaca: "
+    liczbaKodujaca <- getLine
+    putStr "Wczytano: "
+    putStrLn (liczbaKodujaca)
+
+
+    let num = read liczbaKodujaca :: Int
     let length1 = length txt
     putStr "Dlugosc naszego ciagu: "
     print length1
     let lista = map (\x -> [x]) txt --liata cyfr
     putStr "Tak wyglada nasza lista: "
     print lista
-    
+
+    let lista2 = fmap(\x -> read x::Int)lista
+    putStr "Tak wyglada nasza lista jako lista Intów: "
+    print lista2
+
+    let lista3 = fmap(\x -> (x + num) `mod`  10)lista2
+    putStr "Tak wyglada nasza lista Intów po zakodowaniu: "
+    print lista3
